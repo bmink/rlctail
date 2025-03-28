8908e1d939385032d4d162e83aec97599fd5807c
bmink <barnamink@gmail.com>
2025-03-27 23:32:17 -0700

    .

--- reddit.h
+++ reddit.h
@@ -5,7 +5,6 @@
 #include "barr.h"
 #include <time.h>
 
-
 typedef struct reddit_comment {
 	bstr_t	*rc_id;
 	bstr_t	*rc_author;
@@ -13,12 +12,8 @@ typedef struct reddit_comment {
 	time_t	rc_retrieved;
 } reddit_comment_t;
 
-
-
 int reddit_init(const char *, const char *, const char *);
-
 int reddit_get_new_comments(const char *, const char *, barr_t *, const int);
-
 void reddit_uninit(void);
 
 #endif
